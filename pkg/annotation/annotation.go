1cac2f973cde1ebd59488b505ce9b2643c60718b
Kensei Nakada <handbomusic@gmail.com>
2023-04-03 12:49:49 +0900

    delete everything

--- pkg/annotation/annotation.go
+++
@@ -1,6 +0,0 @@
-package annotation
-
-const (
-	HPAContainerBasedCPUExternalMetricNamePrefixAnnotation    = "tortoises.autoscaling.sanposhiho.com/container-based-cpu-metric-prefix"
-	HPAContainerBasedMemoryExternalMetricNamePrefixAnnotation = "tortoises.autoscaling.sanposhiho.com/container-based-memory-metric-prefix"
-)
